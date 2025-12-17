0d5819031b29da67794ed29cc594d932cb5988bc
Goga-ton <goga_ton@mail.ru>
2025-12-17 11:21:23 +0300

    DZ_TG05

---
+++ Тема_11-Aigram(-TG-)/TG05_Aiogram(API)/cats.py
@@ -0,0 +1,57 @@
+import asyncio
+import requests
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
+#ссылка на конфиг, т.к. он лежит не в текущем каталоге
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import config
+
+bot = Bot(token=config.tltok)
+dp = Dispatcher()
+
+def get_cat_breeds():
+    url = 'https://api.thecatapi.com/v1/breeds'
+    headers = {'x-api-key' : config.cat_apikey}
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+def get_cat_img_bread(bread_id):
+    url = f'https://api.thecatapi.com/v1/images/search?breed_ids={bread_id}'
+    headers = {'x-api-key' : config.cat_apikey}
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    return data[0]['url']
+
+def get_bread_info(bread_name):
+    breads = get_cat_breeds()
+    for bread in breads:
+        # print(f'{bread["name"]}')
+        if bread['name'].lower() == bread_name.lower():
+            return bread
+    return None
+
+@dp.message(CommandStart())
+async def start(message:Message):
+    await message.answer('Привет напиши мне название породы кошки и я пришлю ее фотку и информацию о ней')
+
+@dp.message()
+async def send_cat_info(message:Message):
+    bread_name = message.text
+    bread_info = get_bread_info(bread_name)
+    if bread_info:
+        cat_image = get_cat_img_bread(bread_info['id'])
+        info = (f'Порода - {bread_info["name"]}\n'
+                f'Описание - {bread_info["description"]}\n'
+                f'Продолжительность жизни - {bread_info["life_span"]} лет')
+        await message.answer_photo(photo=cat_image, caption=info)
+    else:
+        await message.answer('Порода не найдена')
+
+async def main():
+    await dp.start_polling(bot) #Ваш бот использует polling — программа сама "стучится" к Telegram и распределяет сообщения по хендлерам по фильтрам.
+
+if __name__ == '__main__':
+    asyncio.run(main())
---
+++ Тема_11-Aigram(-TG-)/TG05_Aiogram(API)/nasa.py
@@ -0,0 +1,39 @@
+import asyncio
+import requests
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
+from datetime import datetime, timedelta
+import random
+
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import config
+
+bot = Bot(token=config.tltok)
+dp = Dispatcher()
+
+def get_random_apod():
+    end_date = datetime.now()
+    start_date = end_date - timedelta(days=365)
+    randon_date = start_date + (end_date - start_date) * random.random()
+    date_str = randon_date.strftime("%Y-%m-%d")
+
+    url = f'https://api.nasa.gov/planetary/apod?api_key={config.nasa_apikey}&date={date_str}'
+    response = requests.get(url)
+    return response.json()
+
+@dp.message(Command('fnasa'))
+async def fnasa(message: Message):
+    apod = get_random_apod()
+    photo_url = apod['url']
+    title = apod['title']
+    await message.answer_photo(photo=photo_url, caption=title)
+
+
+async def main():
+    await dp.start_polling(bot) #Ваш бот использует polling — программа сама "стучится" к Telegram и распределяет сообщения по хендлерам по фильтрам.
+
+if __name__ == '__main__':
+    asyncio.run(main())
